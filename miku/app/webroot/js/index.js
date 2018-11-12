fc3025f3411f44f4a352e904411ba697ef7d83b6
Martin <magadiflo@gmail.com>
2018-11-12 01:46:24 -0500

    Se agregaron librerias: jquery, bootstrap, font-awesome, img, fonts

---
+++ miku/app/webroot/js/index.js
@@ -0,0 +1,55 @@
+//SLIDER: Tiempo de transición.
+$('.carousel').carousel({
+    interval: 10000,
+    pause: "hover"
+});
+/*MODAL: Muestra formulario de acceso o registro al sistema*/
+$(".enlace-iniciar-sesion").on("click", function () {
+    $('#cuadro-iniciar-sesion').modal({
+        show: true
+    });
+    limpiaCamposIniciarSesion();
+});
+/*MODAL: Ventana de registro final de nuevo usuario*/
+$("#btn-registrar").on("click", function (e) {
+    e.preventDefault();
+    var emailRegistro = $("#email-registro").val();
+    var terminosCondiciones = $('input:checkbox[name=terminos-condiciones]:checked').val();
+    if (emailRegistro.length <= 0 || !esEmail(emailRegistro)) {
+        $("#grupo-email-registro").addClass("has-error");
+        return;
+    } else if (terminosCondiciones === undefined) {
+        alert("Debe aceptar los términos y condiciones para continuar con el registro.");
+        return;
+    } else {
+        $("#email-comprueba").val(emailRegistro);
+        $('#cuadro-iniciar-sesion')
+            .modal('hide')
+            .on('hidden.bs.modal', function (e) {
+                $('#modal-registro-final').modal('show');
+                $(this).off('hidden.bs.modal');
+            });
+    }
+});
+$("#btn-finalizar-registro").on("click", function (e) {
+    if (!passValidoRegistro()) {
+        e.preventDefault();
+        alert("Las contraseñas no coinciden.");
+        return;
+    }
+});
+function passValidoRegistro() {
+    var pass1 = $("#pass-registro").val();
+    var pass2 = $("#pass-registro-repite").val();
+    return pass1 === pass2;
+}
+function esEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+}
+function limpiaCamposIniciarSesion() {
+    $("#email-acceso").val('');
+    $('#pass-acceso').val('');
+    $("#email-registro").val('');
+    $("#terminos-condiciones").prop('checked', false);
+}
